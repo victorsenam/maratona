279cd872f1e6b0546d2cc2b975cfbad76db34a61
Vinicius Agostini <vinicius.agostini19@gmail.com>
2015-08-05 20:57:07 -0300

    Create bitstrocados.cpp

---
+++ SPOJ/bitstrocados.cpp
@@ -0,0 +1,36 @@
+/*--------------------------------------------------------*/
+/* Problema: Bits Trocados (OBI2000)                      */
+/* Categoria: Greedy                                      */
+/* Data: 05/08/2015                                       */
+/*--------------------------------------------------------*/
+
+
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+#define mset(x,v) memset(x, v, sizeof(x))
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+int main(){
+    int nota[4] = {50, 10, 5, 1};
+    int used[4];
+    int n, ans, t = 0;
+
+    while(scanf("%d", &n) && n != 0){
+        ans = 0; t++;
+        mset(used, 0);
+        for(int i = 0; i < 4; i++){
+            used[i] += n/nota[i];
+            if(used[i] != 0) n -= used[i]*nota[i];
+        }
+
+        printf("Teste %d\n", t);
+        for(int i = 0; i < 4; i++)
+            printf("%d ", used[i]);
+        printf("\n\n");
+    }
+
+}
