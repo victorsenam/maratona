2f890508ce421a41fd886c3986446fc2aee11561
Vinicius Agostini <vinicius.agostini19@gmail.com>
2015-08-06 15:50:20 -0300

    Update darkroads.cpp

--- SPOJ/darkroads.cpp
+++ SPOJ/darkroads.cpp
@@ -1,11 +1,9 @@
-/*--------------------------------------------------------*/
-/* Problema: Dentista (OBI2010, Fase 2, Nível Júnior) */
-/* Categoria: Greedy */
-/* Data: 06/08/2015 */
-/*--------------------------------------------------------*/
+/*----------------------------------------------------------*/
+/* Problema: Dark Roads - ULM09 */
+/* Categoria: Graphs                                        */
+/* Data: 06/08/2015                                         */
+/*----------------------------------------------------------*/
 #include <bits/stdc++.h>
-#define weigth first
-#define vertex second
 #define pb push_back
 #define mp make_pair
 #define mset(x,v) memset(x, v, sizeof(x));
@@ -17,50 +15,51 @@ bool seen[200005];
  
 ll prim(int source){
     set<pair<int,int> > s;
-set<pair<int,int> >::iterator it;
-ll minCost = 0;
+    set<pair<int,int> >::iterator it;
+    ll minCost = 0;
  
-s.insert(mp(0, source));
-while(s.size()){
-//Escolhe a aresta de menor peso
-int x = s.begin()->vertex;
-int w = s.begin()->weigth;
-it = s.begin(); s.erase(it);
- 
-if(seen[x]) continue;
- 
-minCost += w;
-seen[x] = true;
- 
-for(int i = 0; i < adj[x].size(); i++){
-int y = adj[x][i].vertex;
-if(!seen[y])
-s.insert(adj[x][i]);
-}
-}
-return minCost;
+    s.insert(mp(0, source));
+    while(s.size()){
+        //Escolhe a aresta de menor peso
+        int x = s.begin()->vertex;
+        int w = s.begin()->weigth;
+        it = s.begin(); s.erase(it);
+         
+        if(seen[x]) continue;
+         
+        minCost += w;
+        seen[x] = true;
+         
+        for(int i = 0; i < adj[x].size(); i++){
+            int y = adj[x][i].vertex;
+            if(!seen[y])
+                s.insert(adj[x][i]);
+        }
+    }
+    
+    return minCost;
 }
  
 int main(){
-int m, n, x, y, z;
-ll ini;
- 
-while(scanf("%d %d", &m, &n) && m != 0){
-ini = 0;
-mset(seen, 0);
- 
-for(int i = 0; i <= n; i++)
-adj[i].clear();
- 
-while(n--){
-scanf("%d %d %d", &x, &y, &z);
-ini += z;
-adj[x].pb(mp(z,y));
-adj[y].pb(mp(z,x));
-}
- 
-ll ans = ini - prim(1);
-printf("%lld\n", ans);
-}
+    int m, n, x, y, z;
+    ll ini;
+     
+    while(scanf("%d %d", &m, &n) && m != 0){
+        ini = 0;
+        mset(seen, 0);
+         
+        for(int i = 0; i <= n; i++)
+            adj[i].clear();
+             
+        while(n--){
+            scanf("%d %d %d", &x, &y, &z);
+            ini += z;
+            adj[x].pb(mp(z,y));
+            adj[y].pb(mp(z,x));
+        }
+         
+        ll ans = ini - prim(1);
+        printf("%lld\n", ans);
+    }
  
 } 
